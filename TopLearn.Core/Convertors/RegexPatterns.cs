d7bbd281260d535edaee447b8ec69e453465f38b
ali taami <alitaami81@gmail.com>
2024-01-28 01:08:31 +0330

    Added regex validation to views

---
+++ TopLearn.Core/Convertors/RegexPatterns.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TopLearn.Core.Convertors
+{
+    // RegexPatterns.cs 
+    public static class RegexPatterns
+    {
+        public const string EmailPattern = @"^\S+@\S+\.\S+$";
+        public const string PasswordPattern = @"^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[^\da-zA-Z]).{8,}$";
+    }
+
+}
